2b65ac0a8de9741bf02c2feffb0d76dfd8abfd3c
skoanton <antonhakanssonn@gmail.com>
2025-01-22 19:43:16 +0100

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+function App() {
+
+
+  return (
+    <>
+       <Button>Click me</Button>
+       <p className="text-red-500">Tjo bram</p>
+    </>
+  )
+}
+
+export default App
